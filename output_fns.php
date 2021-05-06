1ce1be241535f9a2c4483944e006b2033986ac1e
ericejennings <ejennings25@ivytech.edu>
2021-05-06 14:23:03 -0400

    Wordpress upload
    Wordpress upload

--- output_fns.php
+++ output_fns.php
@@ -4,26 +4,47 @@ function do_html_header($title) {
   // print an HTML header
 ?>
 <!doctype html>
-  <html>
-  <head>
-    <meta charset="utf-8">
-    <title><?php echo $title;?></title>
-    <style>
-      body { font-family: Arial, Helvetica, sans-serif; font-size: 13px }
-      li, td { font-family: Arial, Helvetica, sans-serif; font-size: 13px }
-      hr { color: #3333cc;}
-      a { color: #000 }
-      div.formblock
-         { background: #ccc; width: 300px; padding: 6px; border: 1px solid #000;}
-    </style>
-  </head>
-  <body>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title><?php echo $title;?></title>
+  <style>
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 13px
+  }
+
+  li,
+  td {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 13px
+  }
+
+  hr {
+    color: #3333cc;
+  }
+
+  a {
+    color: #000
+  }
+
+  div.formblock {
+    background: #ccc;
+    width: 300px;
+    padding: 6px;
+    border: 1px solid #000;
+  }
+  </style>
+</head>
+
+<body>
   <div>
     <img src="bookmark.gif" alt="PHPbookmark logo" height="55" width="57" style="float: left; padding-right: 6px;" />
-      <h1>PHPbookmark</h1>
+    <h1>PHPbookmark</h1>
   </div>
   <hr />
-<?php
+  <?php
   if($title) {
     do_html_heading($title);
   }
@@ -32,88 +53,91 @@ function do_html_header($title) {
 function do_html_footer() {
   // print an HTML footer
 ?>
-  </body>
-  </html>
+</body>
+
+</html>
 <?php
 }
 
 function do_html_heading($heading) {
   // print heading
 ?>
-  <h2><?php echo $heading;?></h2>
+<h2><?php echo $heading;?></h2>
 <?php
 }
 
 function do_html_URL($url, $name) {
   // output URL as link and br
 ?>
-  <br><a href="<?php echo $url;?>"><?php echo $name;?></a><br>
+<br><a href="<?php echo $url;?>"><?php echo $name;?></a><br>
 <?php
 }
 
 function display_site_info() {
   // display some marketing info
 ?>
-  <ul>
+<ul>
   <li>Store your bookmarks online with us!</li>
   <li>See what other users use!</li>
   <li>Share your favorite links with others!</li>
-  </ul>
+</ul>
 <?php
 }
 
 function display_login_form() {
 ?>
-  <p><a href="register_form.php">Not a member?</a></p>
-  <form method="post" action="member.php">
+<p><a href="register_form.php">Not a member?</a></p>
+<form method="post" action="member.php">
 
   <div class="formblock">
     <h2>Members Log In Here</h2>
 
-    <p><label for="username">Username:</label><br/>
-    <input type="text" name="username" id="username" /></p>
+    <p><label for="username">Username:</label><br />
+      <input type="text" name="username" id="username" />
+    </p>
 
-    <p><label for="passwd">Password:</label><br/>
-    <input type="password" name="passwd" id="passwd" /></p>
+    <p><label for="passwd">Password:</label><br />
+      <input type="password" name="passwd" id="passwd" />
+    </p>
 
     <button type="submit">Log In</button>
 
     <p><a href="forgot_form.php">Forgot your password?</a></p>
   </div>
 
- </form>
+</form>
 <?php
-}
 
+}
 function display_registration_form() {
 ?>
- <form method="post" action="register_new.php">
+<form method="post" action="register_new.php">
 
- <div class="formblock">
+  <div class="formblock">
     <h2>Register Now</h2>
 
-    <p><label for="email">Email Address:</label><br/>
-    <input type="email" name="email" id="email" 
-      size="30" maxlength="100" required /></p>
+    <p><label for="email">Email Address:</label><br />
+      <input type="email" name="email" id="email" size="30" maxlength="100" required />
+    </p>
 
-    <p><label for="username">Preferred Username <br>(max 16 chars):</label><br/>
-    <input type="text" name="username" id="username" 
-      size="16" maxlength="16" required /></p>
+    <p><label for="username">Preferred Username <br>(max 16 chars):</label><br />
+      <input type="text" name="username" id="username" size="16" maxlength="16" required />
+    </p>
 
-    <p><label for="passwd">Password <br>(between 6 and 16 chars):</label><br/>
-    <input type="password" name="passwd" id="passwd" 
-      size="16" maxlength="16" required /></p>
+    <p><label for="passwd">Password <br>(between 6 and 16 chars):</label><br />
+      <input type="password" name="passwd" id="passwd" size="16" maxlength="16" required />
+    </p>
 
-    <p><label for="passwd2">Confirm Password:</label><br/>
-    <input type="password" name="passwd2" id="passwd2" 
-      size="16" maxlength="16" required /></p>
+    <p><label for="passwd2">Confirm Password:</label><br />
+      <input type="password" name="passwd2" id="passwd2" size="16" maxlength="16" required />
+    </p>
 
 
     <button type="submit">Register</button>
 
-   </div>
+  </div>
 
-  </form>
+</form>
 <?php
 
 }
@@ -125,10 +149,10 @@ function display_user_urls($url_array) {
   global $bm_table;
   $bm_table = true;
 ?>
-  <br>
-  <form name="bm_table" action="delete_bms.php" method="post">
+<br>
+<form name="bm_table" action="delete_bms.php" method="post">
   <table width="300" cellpadding="2" cellspacing="0">
-  <?php
+    <?php
   $color = "#cccccc";
   echo "<tr bgcolor=\"".$color."\"><td><strong>Bookmark</strong></td>";
   echo "<td><strong>Delete?</strong></td></tr>";
@@ -150,7 +174,7 @@ function display_user_urls($url_array) {
   }
 ?>
   </table>
-  </form>
+</form>
 <?php
 }
 
@@ -182,16 +206,16 @@ function display_add_bm_form() {
 ?>
 <form name="bm_table" action="add_bms.php" method="post">
 
- <div class="formblock">
+  <div class="formblock">
     <h2>New Bookmark</h2>
 
     <p>
-    <input type="text" name="new_url" id="new_url" 
-      size="40"  maxlength="255" value="http://" required /></p>
+      <input type="text" name="new_url" id="new_url" size="40" maxlength="255" value="http://" required />
+    </p>
 
     <button type="submit">Add Bookmark</button>
 
-   </div>
+  </div>
 
 </form>
 <?php
@@ -200,59 +224,59 @@ function display_add_bm_form() {
 function display_password_form() {
   // display html change password form
 ?>
-   <br>
-   <form action="change_passwd.php" method="post">
+<br>
+<form action="change_passwd.php" method="post">
 
- <div class="formblock">
+  <div class="formblock">
     <h2>Change Password</h2>
 
-    <p><label for="old_passwd">Old Password:</label><br/>
-    <input type="password" name="old_passwd" id="old_passwd" 
-      size="16" maxlength="16" required /></p>
+    <p><label for="old_passwd">Old Password:</label><br />
+      <input type="password" name="old_passwd" id="old_passwd" size="16" maxlength="16" required />
+    </p>
 
-    <p><label for="passwd2">New Password:</label><br/>
-    <input type="password" name="new_passwd" id="new_passwd" 
-      size="16" maxlength="16" required /></p>
+    <p><label for="passwd2">New Password:</label><br />
+      <input type="password" name="new_passwd" id="new_passwd" size="16" maxlength="16" required />
+    </p>
 
-    <p><label for="passwd2">Repeat New Password:</label><br/>
-    <input type="password" name="new_passwd2" id="new_passwd2" 
-      size="16" maxlength="16" required /></p>
+    <p><label for="passwd2">Repeat New Password:</label><br />
+      <input type="password" name="new_passwd2" id="new_passwd2" size="16" maxlength="16" required />
+    </p>
 
 
     <button type="submit">Change Password</button>
 
-   </div>
-   <br>
-<?php
+  </div>
+  <br>
+  <?php
 }
 
 function display_forgot_form() {
   // display HTML form to reset and email password
 ?>
-   <br>
-   <form action="forgot_passwd.php" method="post">
+  <br>
+  <form action="forgot_passwd.php" method="post">
 
- <div class="formblock">
-    <h2>Forgot Your Password?</h2>
+    <div class="formblock">
+      <h2>Forgot Your Password?</h2>
 
-    <p><label for="username">Enter Your Username:</label><br/>
-    <input type="text" name="username" id="username" 
-      size="16" maxlength="16" required /></p>
+      <p><label for="username">Enter Your Username:</label><br />
+        <input type="text" name="username" id="username" size="16" maxlength="16" required />
+      </p>
 
-    <button type="submit">Change Password</button>
+      <button type="submit">Change Password</button>
 
-   </div>
-   <br>
-<?php
+    </div>
+    <br>
+    <?php
 }
 
 function display_recommended_urls($url_array) {
   // similar output to display_user_urls
   // instead of displaying the users bookmarks, display recomendation
 ?>
-  <br>
-  <table width="300" cellpadding="2" cellspacing="0">
-<?php
+    <br>
+    <table width="300" cellpadding="2" cellspacing="0">
+      <?php
   $color = "#cccccc";
   echo "<tr bgcolor=\"".$color."\">
         <td><strong>Recommendations</strong></td></tr>";
@@ -270,8 +294,8 @@ function display_recommended_urls($url_array) {
     echo "<tr><td>No recommendations for you today.</td></tr>";
   }
 ?>
-  </table>
-<?php
+    </table>
+    <?php
 }
 
 ?>
